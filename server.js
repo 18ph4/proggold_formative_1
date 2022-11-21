d97a532a9d8aea2ebca02400528d13c9290b18b6
18ph4git config--global user.email nfvq59@durham.ac.uk <nfvq59@durham.ac.uk>
2022-11-21 23:27:55 +0000

    provied c3 static module

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('static'))
+app.use('/node_modules/c3', express.static('node_modules/c3'))
 app.use('/node_modules/papaparse', express.static('node_modules/papaparse'))
-
 
 app.listen(80)
