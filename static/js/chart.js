7750bc14c0c7fea4da83f13011c969511a35ef9b
18ph4git config--global user.email nfvq59@durham.ac.uk <nfvq59@durham.ac.uk>
2022-11-22 00:17:23 +0000

    Working chart generation; viewing not implemented

--- static/js/chart.js
+++ static/js/chart.js
@@ -3,50 +3,75 @@ async function parseData(createG) {
     let response = await fetch("salaries_cyber.csv");
     let text_csv = await response.text();
     let dataset = Papa.parse(text_csv)
-    return dataset
+    return dataset.data
     
 }
 
-function createG(data){
-    var years = ['work_year'];
-    var salary = ['salary'];
-    var jobs = ['job_title']
-    for (var i = 1; i < data.length; i ++){
-        years.push(data[i][0]);
-        jobs.push(data[i][2])
-        salary.push(data[i][3]); 
+function castValue(field, type) {
+    if (type === "int") {
+        return parseInt(field)
     }
+    if (type === "str") {
+        return field
+    }
+    throw new Error("Unsupported field type.")
+}
 
-    var chart = c3.generate({
+function rowsToColumns(data){
+    let columns = {};
+    let column_titles = data[0];
+    let column_types = data[1];
+
+    data[0].forEach(function(element){columns[element] = []});
+
+    for (var i = 2; i < data.length; i ++){
+        let row = data[i];
+        for (var row_i = 0; row_i < row.length; row_i++){
+            let field_name = data[0][row_i];
+            let field_value = row[row_i];
+            field_value = castValue(field_value, column_types[row_i])
+
+            columns[field_name].push(field_value)
+        }
+    }
+    console.log(columns)
+    return columns;
+}
+
+function createGraph(data){
+    let columns = rowsToColumns(data);
+
+    let chart_data = {
         data: {
             xs: {
-                setosa: 'setosa_x',
-                versicolor: 'versicolor_x',
+                salary_in_usd: "work_year"
             },
-            // iris data from R
             columns: [
-                jobs,
-                salary
+                ["salary_in_usd"].concat(columns["salary_in_usd"]),
+                ["work_year"].concat(columns["work_year"])
             ],
             type: 'scatter'
         },
         axis: {
             x: {
-                label: 'Sepal.Width',
+                label: 'Work Year',
                 tick: {
                     fit: false
                 }
             },
             y: {
-                label: 'Petal.Width'
+                label: 'Salary'
             }
         }
-    });
+    }
+
+    var chart = c3.generate(chart_data);
+
+    console.log(chart);
 
    return chart
-    
 }
 
-parseData();
+parseData().then((data)=>{createGraph(data)})
 
 //https://www.papaparse.com/
