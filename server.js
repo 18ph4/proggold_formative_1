0294b9eed79c6e5890328cb634aa2a7988fad153
18ph4git config--global user.email nfvq59@durham.ac.uk <nfvq59@durham.ac.uk>
2022-11-22 00:16:44 +0000

    serve whole node modules directory

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('static'))
-app.use('/node_modules/c3', express.static('node_modules/c3'))
-app.use('/node_modules/papaparse', express.static('node_modules/papaparse'))
+app.use('/node_modules', express.static('node_modules'))
 
 app.listen(80)
