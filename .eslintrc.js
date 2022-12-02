07f268221edd8995b24b57d46078eab1e0438333
18ph4git config--global user.email nfvq59@durham.ac.uk <nfvq59@durham.ac.uk>
2022-12-02 13:48:09 +0000

    changed eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,7 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
+    "extends": "standard",
     "rules": {
+        "semi": [2, "always"],
+        "indent": "off"
     }
 }
