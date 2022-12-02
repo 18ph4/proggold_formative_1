31c53399aebe8c4176976d986753d973f736daaf
18ph4git config--global user.email nfvq59@durham.ac.uk <nfvq59@durham.ac.uk>
2022-12-02 14:47:20 +0000

    changed the rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,17 @@
 module.exports = {
-    "extends": "standard",
-    "rules": {
-        "semi": [2, "always"],
-        "indent": "off"
-    }
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'standard',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    "semi": [2, "always"],
+    "indent": "off"
+  }
 }
